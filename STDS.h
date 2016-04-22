254af8b55ba6ab72d05d254a4890721aa48c4b03
osra1206 <osra1707@gmail.com>
2016-04-22 10:14:16 +0800

    Create STDS.h

---
+++ STDS.h
@@ -0,0 +1,4 @@
+// Do not forget to type ";"
+// Note that the function prototype must be coherent with the 
+// declaration in FunctionName.cpp
+long double *STDS(int, long double *, long double *, long double *, long double *);
